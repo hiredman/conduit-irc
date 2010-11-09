b1c13bd3ca229c5f86778730d635aab6aa924778
Kevin Downey (hiredman) <redchin@gmail.com>
2010-11-08 17:22:19 -0800

    release!

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject conduit-irc "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject conduit-irc "1.0.0"
+  :description "irc over conduit"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
                  [pircbot/pircbot "1.4.2"]
                  [net.intensivesystems/conduit "0.7.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
