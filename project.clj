d4d14c230aba03f60a69d1ce3ca8c536b823a273
Kevin Downey (hiredman) <redchin@gmail.com>
2011-02-24 13:59:22 -0800

    back to SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject conduit-irc "1.0.0"
+(defproject conduit-irc "1.0.1-SNAPSHOT"
   :description "irc over conduit"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [pircbot/pircbot "1.4.2"]
